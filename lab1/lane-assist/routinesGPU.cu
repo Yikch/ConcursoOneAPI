1252e8e8221dc0645c5b2d4980543289e14e072e
Yikch <99170695+Yikch@users.noreply.github.com>
2023-03-24 15:42:33 +0100

    Add files via upload

---
+++ lab1/lane-assist/routinesGPU.cu
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <cuda.h>
+
+#include "routinesGPU.h"
+
+#define NTHREADS 32
+#define TILE_DIM 5
+
+__global__ void gpu_canny(uint8_t *im, uint8_t *image_out,
+	float *NR, float *G, float *phi, float *Gx, float *Gy, uint8_t *pedge,
+	float level, int height, int width)
+{
+	int i, j; 
+	__shared__ float tile [ TILE_DIM ] [ TILE_DIM+1 ]; 
+	i = blockIdx.y * blockDim.y + threadIdx.y; 
+	j = blockIdx.x * blockDim.x + threadIdx.x; 
+}
+__global__ void gpu_hough(uint8_t *im, int width, int height, uint32_t *accumulators, int accu_width, int accu_height, 
+	float *sin_table, float *cos_table)
+{
+
+}
+
+void getlines(int threshold, uint32_t *accumulators, int accu_width, int accu_height, int width, int height, 
+	float *sin_table, float *cos_table,
+	int *x1_lines, int *y1_lines, int *x2_lines, int *y2_lines, int *lines)
+{
+	int rho, theta, ii, jj;
+	uint32_t max;
+
+	for(rho=0;rho<accu_height;rho++)
+	{
+		for(theta=0;theta<accu_width;theta++)  
+		{  
+
+			if(accumulators[(rho*accu_width) + theta] >= threshold)  
+			{  
+				//Is this point a local maxima (9x9)  
+				max = accumulators[(rho*accu_width) + theta]; 
+				for(int ii=-4;ii<=4;ii++)  
+				{  
+					for(int jj=-4;jj<=4;jj++)  
+					{  
+						if( (ii+rho>=0 && ii+rho<accu_height) && (jj+theta>=0 && jj+theta<accu_width) )  
+						{  
+							if( accumulators[((rho+ii)*accu_width) + (theta+jj)] > max )  
+							{
+								max = accumulators[((rho+ii)*accu_width) + (theta+jj)];
+							}  
+						}  
+					}  
+				}  
+
+				if(max == accumulators[(rho*accu_width) + theta]) //local maxima
+				{
+					int x1, y1, x2, y2;  
+					x1 = y1 = x2 = y2 = 0;  
+
+					if(theta >= 45 && theta <= 135)  
+					{
+						if (theta>90) {
+							//y = (r - x cos(t)) / sin(t)  
+							x1 = width/2;  
+							y1 = ((float)(rho-(accu_height/2)) - ((x1 - (width/2) ) * cos_table[theta])) / sin_table[theta] + (height / 2);
+							x2 = width;  
+							y2 = ((float)(rho-(accu_height/2)) - ((x2 - (width/2) ) * cos_table[theta])) / sin_table[theta] + (height / 2);  
+						} else {
+							//y = (r - x cos(t)) / sin(t)  
+							x1 = 0;  
+							y1 = ((float)(rho-(accu_height/2)) - ((x1 - (width/2) ) * cos_table[theta])) / sin_table[theta] + (height / 2);
+							x2 = width*2/5;  
+							y2 = ((float)(rho-(accu_height/2)) - ((x2 - (width/2) ) * cos_table[theta])) / sin_table[theta] + (height / 2); 
+						}
+					} else {
+						//x = (r - y sin(t)) / cos(t);  
+						y1 = 0;  
+						x1 = ((float)(rho-(accu_height/2)) - ((y1 - (height/2) ) * sin_table[theta])) / cos_table[theta] + (width / 2);  
+						y2 = height;  
+						x2 = ((float)(rho-(accu_height/2)) - ((y2 - (height/2) ) * sin_table[theta])) / cos_table[theta] + (width / 2);  
+					}
+					x1_lines[*lines] = x1;
+					y1_lines[*lines] = y1;
+					x2_lines[*lines] = x2;
+					y2_lines[*lines] = y2;
+					(*lines)++;
+				}
+			}
+		}
+	}
+}
+
+
+void line_asist_GPU(uint8_t *im, int height, int width,
+	uint8_t *imEdge, float *NR, float *G, float *phi, float *Gx, float *Gy, uint8_t *pedge,
+	float *sin_table, float *cos_table, 
+	uint32_t *accum, int accu_height, int accu_width,
+	int *x1, int *x2, int *y1, int *y2, int *nlines)
+{
+	int threshold;
+
+	/* CUDA vesion */
+
+	dim3 dimBlock(NTHREADS,NTHREADS);
+	int blocks = (height*width/25)/NTHREADS;
+	if ((height*width/25)%NTHREADS>0) blocks++;
+	dim3 dimGrid(blocks, blocks);
+
+	gpu_canny<<<dimGrid,dimBlock>>>(im, imEdge, NR, G, phi, Gx, Gy, pedge,
+		1000.0f, //level
+		height, width);	
+	cudaDeviceSynchronize();
+	
+	getlines(threshold, accum, accu_width, accu_height, width, height, 
+		sin_table, cos_table,
+		x1, y1, x2, y2, nlines);
+}
